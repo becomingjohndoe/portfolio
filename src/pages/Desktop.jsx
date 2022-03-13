db3d8fe2cd799fddbc87bdec6d71424a1c748d8b
becomingjohndoe <karl.swatman@gmail.com>
2022-03-13 21:08:22 +0000

    fix bug

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -36,7 +36,22 @@ class Desktop extends Component {
 					<div className="body">
 						<p>This is the modal&apos;s body.</p>
 					</div>
-					<button onClick={this.closeModal}>Close modal</button>
+					<button onClick={this.closeModalOne}>Close modal</button>
+				</ReactModal>
+				<button onClick={this.openModalOne}>Open modal</button>
+				<ReactModal
+					initWidth={800}
+					initHeight={400}
+					onFocus={() => console.log("Modal is clicked")}
+					className={"my-modal-custom-class"}
+					onRequestClose={this.closeModalOne}
+					isOpen={this.state.modalOneIsOpen}
+				>
+					<h3>My Modal</h3>
+					<div className="body">
+						<p>This is the modal&apos;s body.</p>
+					</div>
+					<button onClick={this.closeModalOne}>Close modal</button>
 				</ReactModal>
 			</div>
 		);

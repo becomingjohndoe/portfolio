868035d6b85bcdf2b282aab5b621d5933625c389
becomingjohndoe <karl.swatman@gmail.com>
2022-03-15 23:08:33 +0000

    create about section

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+	return (
+		<div class="container">
+			<div class="grid grid-cols-1  h-200">
+				<div class="flex bg-gray-100 p-10">
+					<div class="mb-auto mt-auto max-w-lg">
+						<h1 class="text-3xl uppercase">Jun Santilla</h1>
+						<p class="font-semibold mb-5">Web Developer</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+							Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
+							Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+						</p>
+						<button class="bg-black rounded-md py-3 px-7 mt-6 text-white">
+							Email Me
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default About;

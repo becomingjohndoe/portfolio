20d05dc0736a47add8ae3fe25edf4dc3e1998c26
becomingjohndoe <karl.swatman@gmail.com>
2022-03-15 00:34:09 +0000

    set up basic icons on desktop

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -1,61 +1,21 @@
-import React, { Component } from "react";
-import ReactModal from "react-modal-resizable-draggable";
+import Icon from "../components/Icon";
 
-class Desktop extends Component {
-	constructor() {
-		super();
+const iconStyle = {
+	display: "flex",
+	flexdirection: "column",
+};
 
-		this.state = {
-			modalIsOpen: false,
-		};
-
-		this.openModal = this.openModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
-	}
-
-	openModal() {
-		this.setState({ modalIsOpen: true });
-	}
-	closeModal() {
-		this.setState({ modalIsOpen: false });
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.openModal}>Open modal</button>
-				<ReactModal
-					initWidth={800}
-					initHeight={400}
-					onFocus={() => console.log("Modal is clicked")}
-					className={"my-modal-custom-class"}
-					onRequestClose={this.closeModal}
-					isOpen={this.state.modalIsOpen}
-				>
-					<h3>My Modal</h3>
-					<div className="body">
-						<p>This is the modal&apos;s body.</p>
-					</div>
-					<button onClick={this.closeModalOne}>Close modal</button>
-				</ReactModal>
-				<button onClick={this.openModalOne}>Open modal</button>
-				<ReactModal
-					initWidth={800}
-					initHeight={400}
-					onFocus={() => console.log("Modal is clicked")}
-					className={"my-modal-custom-class"}
-					onRequestClose={this.closeModalOne}
-					isOpen={this.state.modalOneIsOpen}
-				>
-					<h3>My Modal</h3>
-					<div className="body">
-						<p>This is the modal&apos;s body.</p>
-					</div>
-					<button onClick={this.closeModalOne}>Close modal</button>
-				</ReactModal>
-			</div>
-		);
-	}
-}
+const Desktop = () => {
+	return (
+		<div style={iconStyle}>
+			<Icon />
+			<Icon />
+			<Icon />
+			<Icon />
+			<Icon />
+			<Icon />
+		</div>
+	);
+};
 
 export default Desktop;

dea3dbab836a2d621f4a9e85a33aa264acc53a82
becomingjohndoe <karl.swatman@gmail.com>
2022-03-17 02:35:48 +0000

    first iteration of desktop component

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -1,28 +1,38 @@
 import { Header } from "../components/Header";
 import Icon from "../components/Icon";
+import MusicPlayer from "../components/MusicPlayer";
+import Settings from "../components/Settings";
 import About from "./About";
-
-const iconStyle = {
-	display: "flex",
-	height: "100vh",
-	width: "200px",
-	flexFlow: "column wrap",
-	paddingBottom: "100px",
-};
 
 const Desktop = () => {
 	return (
-		<div style={iconStyle}>
-			<Icon>
-				<About />{" "}
-			</Icon>
+		<>
+			{/* icon bar */}
+			<div className="h-16 w-full bg-slate-700 absolute bottom-2 z-50 opacity-80 px-4"></div>
+			{/* desktop */}
+			<div className="relative h-full justify-center flex flex-row bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+				{/* icon set */}
+				<Icon
+					name="About Me"
+					image="https://cdn-icons-png.flaticon.com/512/4825/4825044.png"
+				>
+					<About />{" "}
+				</Icon>
 
-			<Icon />
-			<Icon />
-			<Icon />
-			<Icon />
-			<Icon />
-		</div>
+				<Icon
+					name="Music"
+					image="https://cdn-icons-png.flaticon.com/512/4497/4497920.png"
+				>
+					<MusicPlayer />
+				</Icon>
+				<Icon
+					name="Settings"
+					image="https://cdn-icons-png.flaticon.com/512/5045/5045224.png"
+				>
+					<Settings />
+				</Icon>
+			</div>
+		</>
 	);
 };
 

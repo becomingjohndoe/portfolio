0d4961a820962b11eaa2ee67e6d704e780ad0a96
becomingjohndoe <karl.swatman@gmail.com>
2022-03-17 02:36:02 +0000

    first iteration of about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,30 @@
+import headshot from "../assets/images/headshot.png";
+
 const About = () => {
 	return (
-		<div class="container">
-			<div class="grid grid-cols-1  h-200">
-				<div class="flex bg-gray-100 p-10">
-					<div class="mb-auto mt-auto max-w-lg">
-						<h1 class="text-3xl uppercase">Jun Santilla</h1>
-						<p class="font-semibold mb-5">Web Developer</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
-							Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
-							Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-						</p>
-						<button class="bg-black rounded-md py-3 px-7 mt-6 text-white">
-							Email Me
-						</button>
-					</div>
-				</div>
+		<figure className=" md:flex bg-gradient-to-r from-cyan-500 to-orange-100 pl-6 dark:from-slate-500 dark:to-teal-900 m-0">
+			<img className=" w-32 h-32 rounded-full m-auto" src={headshot} alt="" />
+			<div className="p-8 text-center md:text-left">
+				<blockquote>
+					<p className="text-lg text-left font-medium overflow-scroll max-h-48 max-w-sm dark:text-slate-100">
+						“Tailwind CSS is the only framework that I've seen scale on large teams.
+						It’s easy to customize, adapts to any design, and the build size is tiny.”
+						“Tailwind CSS is the only framework that I've seen scale on large teams.
+						It’s easy to customize, adapts to any design, and the build size is tiny.”
+						“Tailwind CSS is the only framework that I've seen scale on large teams.
+						It’s easy to customize, adapts to any design, and the build size is tiny.”
+						“Tailwind CSS is the only framework that I've seen scale on large teams.
+						It’s easy to customize, adapts to any design, and the build size is tiny.”
+						“Tailwind CSS is the only framework that I've seen scale on large teams.
+						It’s easy to customize, adapts to any design, and the build size is tiny.”
+					</p>
+				</blockquote>
+				<figcaption className="font-medium text-left">
+					<div className="text-sky-500 dark:text-sky-400">Karl Swatman</div>
+					<div className="text-slate-700 dark:text-sky-500">Front End Developer</div>
+				</figcaption>
 			</div>
-		</div>
+		</figure>
 	);
 };
 

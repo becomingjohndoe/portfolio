7298186a034b19bbfae82109997212420d46a33c
becomingjohndoe <karl.swatman@gmail.com>
2022-03-21 22:10:56 +0000

    add skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,203 @@
+import { Link } from "react-router-dom";
+
+const Skills = () => {
+	return (
+		<div className="max-w-md p-2 content-center bg-indigo-400 max-h-full">
+			<button className="h-12 bg-indigo-500 mb-3 p-2 rounded-sm text-slate-50 font-bold">
+				<a href="/somefile.txt" download>
+					Download CV
+				</a>
+			</button>
+			<p className="flex flex-wrap items-center justify-center gap-5 border p-2 rounded-sm overflow-scroll h-64 text-sm text-slate-50">
+				{" "}
+				<a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+						alt="bootstrap"
+						width="60"
+						height="60"
+					/>
+					Boostrap
+				</a>{" "}
+				<a href="https://www.chartjs.org" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.chartjs.org/media/logo-title.svg"
+						alt="chartjs"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+						alt="css3"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://expressjs.com" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+						alt="express"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+						alt="firebase"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://cloud.google.com" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg"
+						alt="gcp"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+						alt="git"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://heroku.com" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
+						alt="heroku"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+						alt="html5"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a
+					href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+						alt="javascript"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://jestjs.io" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+						alt="jest"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+						alt="mongodb"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://nodejs.org" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+						alt="nodejs"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+						alt="postgresql"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.python.org" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+						alt="python"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+						alt="react"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://reactnative.dev/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://reactnative.dev/img/header_logo.svg"
+						alt="reactnative"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+						alt="tailwind"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.tensorflow.org" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg"
+						alt="tensorflow"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+				<a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
+					{" "}
+					<img
+						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+						alt="typescript"
+						width="60"
+						height="60"
+					/>{" "}
+				</a>{" "}
+			</p>
+		</div>
+	);
+};
+
+export default Skills;

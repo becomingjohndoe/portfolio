83a01e70ffb311fa4ac9b37a840378ccf7adc3fb
becomingjohndoe <karl.swatman@gmail.com>
2022-03-23 19:12:10 +0000

    dark mode toggler

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 const ToggleSwitch = () => {
+	const { theme, setTheme } = useContext(ThemeContext);
 	return (
 		<>
 			{/* <!-- label --> */}
@@ -13,9 +17,15 @@ const ToggleSwitch = () => {
 						{/* <!-- input --> */}
 						<input type="checkbox" id="toggleB" class="sr-only" />
 						{/* <!-- line --> */}
-						<div class="block bg-slate-400 w-14 h-8 rounded-full"></div>
+						<div
+							onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+							class="block bg-slate-400 w-14 h-8 rounded-full"
+						></div>
 						{/* <!-- dot --> */}
-						<div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+						<div
+							onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+							class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"
+						></div>
 					</div>
 				</label>
 			</div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "../src/context/ThemeContext";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<ThemeProvider>
+		<App />
+	</ThemeProvider>,
+	document.getElementById("root")
+);
